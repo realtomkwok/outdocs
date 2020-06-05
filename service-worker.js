bee68be93304e1f4ce403b21b3258bad11530027
Tom Kwok <junhaotom@gmail.com>
2020-06-05 17:19:21 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/realtomkwok.github.io/outdocs/precache-manifest.502e4af7f964872d6bb20fb9398df1ef.js"
+  "/outdocs/precache-manifest.811dc513f238ddc147f91e5e5cedc115.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realtomkwok.github.io/outdocs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/outdocs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
