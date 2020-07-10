3276cc3fcb3997e6c2d1193fe5df0041815bacc4
Tom Kwok <junhaotom@gmail.com>
2020-07-10 11:08:12 +0800

    Updates

---
+++ precache-manifest.5a619393140e7e41539c3a175264a336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69a38caaab3dd30d591fcb8f244c1f0",
+    "url": "/www.outdocs.cn/index.html"
+  },
+  {
+    "revision": "77914c56ddcb6ee97e2b",
+    "url": "/www.outdocs.cn/static/css/main.754ae001.chunk.css"
+  },
+  {
+    "revision": "9bb11bd0b449d21b9fa3",
+    "url": "/www.outdocs.cn/static/js/2.2992225c.chunk.js"
+  },
+  {
+    "revision": "e0e8de7b0757adda7183ab058155ca06",
+    "url": "/www.outdocs.cn/static/js/2.2992225c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77914c56ddcb6ee97e2b",
+    "url": "/www.outdocs.cn/static/js/main.a3796abf.chunk.js"
+  },
+  {
+    "revision": "ce59a7c185e56032b39c",
+    "url": "/www.outdocs.cn/static/js/runtime-main.b134167d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c66bda144b898f5008cfdd960722685.js"
+  "/www.outdocs.cn/precache-manifest.5a619393140e7e41539c3a175264a336.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.outdocs.cn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
