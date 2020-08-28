bbd1dfbe27e73d12b5868a76bfc2ba8b37e29d5f
Tom Kwok <junhaotom@gmail.com>
2020-08-28 11:19:13 +0800

    ⬆️ update dependencies

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
+  ],
+}
+npm
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      bigHeading: [
+        "NimbusSansExtd-Bla",
+        "source-han-sans-simplified-c",
+        "Helvetica Neue",
+        "Helvetica",
+        "system-ui",
+        "sans-serif",
+      ],
+      heading: [
+        "NimbusSansExtd-Bol",
+        "source-han-sans-simplified-c",
+        "Helvetica Neue",
+        "Helvetica",
+        "system-ui",
+        "sans-serif",
+      ],
+      body: [
+        "NimbusSansExtd-Reg",
+        "source-han-sans-simplified-c",
+        "Helvetica Neue",
+        "Helvetica",
+        "system-ui",
+        "sans-serif",
+      ],
+    },
+    color: {
+      accentColor: "#f5ff00",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
