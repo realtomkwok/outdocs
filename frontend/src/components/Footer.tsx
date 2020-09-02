65c9ace6dedbc93c173c39e7d2df48c8039e8bfe
Tom Kwok <junhaotom@gmail.com>
2020-09-03 00:26:48 +0800

    🚩 Add useHover hook and a popup of WeChat QR code

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,11 @@
 import React from "react"
 import tw from "twin.macro"
-import { Link } from "gatsby"
 
 import logo from '../img/logo/outdocs-brand-name.svg'
 import SNSLinks from '../components/SNSLinks'
 
 export default function Footer() {
-  const Container = tw.footer`w-full p-16 absolute bottom-0`
+  const Container = tw.footer`container mx-auto w-full p-16 absolute bottom-0`
   const Row = tw.div`flex items-start justify-between`
   const Logo = tw.img`h-20`
 
--- frontend/src/components/SNSLinks.tsx
+++ frontend/src/components/SNSLinks.tsx
@@ -1,41 +1,52 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
-import { graphql, Link, useStaticQuery } from "gatsby"
-
-import wechat from "../img/social_network/wechat.svg"
+import { graphql, useStaticQuery } from "gatsby"
+import useHover from "../utils/useHover"
 
 type IconProps = {
     url: string
     link: string
     alt: string
-    id: string
 }
 
+type WeChatProps = {
+    iconUrl: string
+    qrCodeUrl: string
+    alt: string
+}
 
-
-// const Icon = tw.object`w-8 ml-6`
-
-const Icon = styled.img`${tw`w-8 ml-6`}`
+const Icon = styled.img`
+    ${tw`w-8 ml-6`}
+`
 
 function SNSIcon(props: IconProps) {
     return (
-        <div tw="hover:scale-50">
-            <Link key={props.id} to={props.link}>
-                <Icon src={props.url} />
-            </Link>
-        </div>
+        <a href={props.link}>
+            <Icon src={props.url} alt={props.alt} />
+        </a>
     )
 }
 
-function WeChat() {
-    return <Icon src={wechat} />
+function WeChat(props: WeChatProps) {
+    const [hoverRef, isHovered] = useHover()
+    const PopUp = tw.img`h-20 w-20`
+
+    return (
+        <div ref={hoverRef}>
+            {isHovered ? (
+                <PopUp src={props.qrCodeUrl}></PopUp>
+            ) : (
+                <Icon src={props.iconUrl} />
+            )}
+        </div>
+    )
 }
 
 export default function SNSLinks() {
     const IconList = tw.div`flex flex-row`
 
-    //📖 Declare for nested objects: https://stackoverflow.com/questions/38245081/nested-objects-in-typescript
-    type platformIconType = {
+    //📖 Declare for nested objects: https://stackoverflow.com/questions/38245081/nested-objects-in-typescriptr
+    type imgType = {
         file: fileType
     }
 
@@ -46,15 +57,16 @@ export default function SNSLinks() {
     type dataType = {
         platform: string
         link: string
-        platformIcon: platformIconType
+        platformIcon: imgType
         id: string
+        qrCode: imgType
     }
 
     const rawSnsData = useStaticQuery(graphql`
-        query SNSData {
-            allContentfulSocialNetworks(
+        query {
+            OtherData: allContentfulSocialNetworks(
                 sort: { fields: order, order: ASC }
-                filter: { node_locale: { eq: "en-US" } }
+                filter: { node_locale: { eq: "en-US" }, order: {ne: 5} }
             ) {
                 nodes {
                     platform
@@ -65,22 +77,51 @@ export default function SNSLinks() {
                         }
                     }
                     id
+                    qrCode {
+                        file {
+                            url
+                        }
+                    }
+                }
+            }
+            WeChatData: allContentfulSocialNetworks(
+                sort: { fields: order, order: ASC }
+                filter: { node_locale: { eq: "en-US" }, order: { eq: 5 } }
+            ) {
+                nodes {
+                    platform
+                    link
+                    platformIcon {
+                        file {
+                            url
+                        }
+                    }
+                    id
+                    qrCode {
+                        file {
+                            url
+                        }
+                    }
                 }
             }
         }
     `)
-    const snsData: Array<dataType> =
-        rawSnsData.allContentfulSocialNetworks.nodes
+    const snsData: Array<dataType> = rawSnsData.OtherData.nodes
+    const WeChatData: Array<dataType> = rawSnsData.WeChatData.nodes
 
     return (
         <IconList>
-            <WeChat />
+            <WeChat
+                iconUrl={WeChatData[0].platformIcon.file.url}
+                qrCodeUrl={WeChatData[0].qrCode.file.url}
+                alt={WeChatData[0].platform}
+            />
             {snsData.map(img => (
                 <SNSIcon
                     url={img.platformIcon.file.url}
                     alt={img.platform}
                     link={img.link}
-                    id={img.id}
+                    key={img.id}
                 />
             ))}
         </IconList>
