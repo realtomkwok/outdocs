f67227aad8f3dd856af61c934286871ee038ef5b
Tom Kwok <junhaotom@gmail.com>
2020-09-03 15:34:35 +0800

    🚩 Finish `footer` component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,21 +1,39 @@
 import React from "react"
-import tw from "twin.macro"
+import tw, { styled } from "twin.macro"
 
-import logo from '../img/logo/outdocs-brand-name.svg'
-import SNSLinks from '../components/SNSLinks'
+import logo from "../img/logo/outdocs-brand-name.svg"
+import SNSLinks from "../components/SNSLinks"
+import SponsorLogos from "../components/SponsorLogos"
+
+function Footnote() {
+    const Text = tw.p``
+    const tomsWebsite: string = "//realtomkwok.github.io/portfolio"
+
+    return (
+        <Text>
+            ©2020 OUTDOCS. Site by <a href={tomsWebsite}>Tom Kwok.</a>{" "}
+        </Text>
+    )
+}
 
 export default function Footer() {
-  const Container = tw.footer`container mx-auto w-full p-16 absolute bottom-0`
-  const Row = tw.div`flex items-start justify-between`
-  const Logo = tw.img`h-20`
+    const Container = tw.footer`container mx-auto w-full p-16 absolute bottom-0 grid grid-cols-1 gap-32`
+    const Row = styled.div<{ itemsStart: boolean }>`
+        ${tw`flex justify-between`} ${({ itemsStart }) =>
+            itemsStart ? tw`items-start` : tw`items-end`}
+    `
+    const Logo = tw.img`h-20 `
 
-  return (
-    <Container>
-      <Row>
-        <Logo src={logo} />
-        <SNSLinks />
-      </Row>
-      <Row></Row>
-    </Container>
-  )
+    return (
+        <Container>
+            <Row itemsStart={true}>
+                <Logo src={logo} />
+                <SNSLinks />
+            </Row>
+            <Row itemsStart={false}>
+                <SponsorLogos />
+                <Footnote />
+            </Row>
+        </Container>
+    )
 }
---
+++ frontend/src/components/SponsorLogos.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import tw from 'twin.macro'
+
+export default function SponsorLogos() {
+    const Container = tw.div`grid grid-cols-1 gap-6`
+    //⚠️ fixed height may be not compatible for every logo
+    const Logos = tw.div`bg-gray-500 h-12`
+    
+    return (
+        <Container>
+            <p>感谢以下有趣机构支持</p>
+            <Logos />
+        </Container>
+    )
+}
